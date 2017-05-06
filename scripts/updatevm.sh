09e6636dc37eec6183db468bcd7395cbab8f14f8
Instant Contiki <user@instant-contiki.(none)>
2017-05-06 15:47:30 +0200

    Added updateVM script, v0.0

---
+++ scripts/updatevm.sh
@@ -0,0 +1,16 @@
+# !/bin/bash
+#
+# Author	: Vitor Rodrigues Di Toro" <vitorrditoro@gmail.com>
+# Created in	: 2017-05-06 
+#
+#---------------------------------------------------------------------
+#
+# History:
+#
+#    v0.0 2017-05-06, Vitor Di Toro:
+#	- Created this script, with just the header.
+#
+#---------------------------------------------------------------------
+#
+# License:
+#    This is free software, licensed under the GNU General Public License v2.
