e180b989b671442feefb6defa41f2546e9181107
Vitor Rodrigues Di Toro <vitorrditoro@gmail.com>
2017-05-21 03:06:40 -0300

    v0.0 - Created file

---
+++ scripts/mobility.sh
@@ -0,0 +1,18 @@
+# !/bin/bash
+#
+# Author        : Vitor Rodrigues Di Toro" <vitorrditoro@gmail.com>
+# Created in    : 2017-05-21
+#
+#-------------------------------------------------------------------------------
+#
+# History
+#
+#    V0.0 2017-05-21, Vitor R. Di Toro:
+#       - Created file
+#       
+#-------------------------------------------------------------------------------
+#
+# License:
+#    This is free software, licensed under the GNU General Public License v2.
+#
+
